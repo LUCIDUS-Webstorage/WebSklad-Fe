6b40b4ff2be0cf92df16626cc1c1daaf814d30fc
Roman <romantomasik2008@gmail.com>
2025-03-13 22:45:40 +0100

    funguje

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router";
 import './App.css';
-//import Dashboard from "./Dashboard";
-import Home2 from "./pages/Home2";
+import Dashboard from "./pages/Dashboard";
+//import Home2 from "./pages/Home2";
 import Login1 from "./pages/Login1";
 //import Header from "./header";
 import Ucet from "./pages/Ucet";
-//import PartsTest from "./PartsTest";
+//import Dashboard from "./pages/Dashboard";
+//import PartsTest from "./pages/PartsTest";
 
 /**
  * Use App.jsx as source for routing information
@@ -14,7 +15,7 @@ import Ucet from "./pages/Ucet";
 function App() {
   return (
      <Routes>
-        <Route index path="/" element={<Home2 />} />
+        <Route index path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login1 />} />
         <Route path="/ucet" element={<Ucet />} />
       </Routes>
